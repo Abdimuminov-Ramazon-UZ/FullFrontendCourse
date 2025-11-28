ca1f79ef425d9654713e809ef7ee5c1e95e5c68f
Abdumuminov Ramozon <generallg5390@gmail.com>
2025-11-28 20:36:55 +0500

    Second Tutorial Of JavaScript

---
+++ JavascriptTutorial(by Samar Badriddinov))/index.js
@@ -0,0 +1,45 @@
+//Tutorial for Samar Badriddinov
+//Important datas
+//differencies of varibles
+//JavaScript is more sensitive in variable names we don't create characteric variables
+// let max = 'anna'
+// let tim = 'jean'
+// let anna = () => {}
+// const maximal = Infinity
+// var age = 26
+// date = 41
+// console.log(date, maximal + 1)
+// const person = {
+// 	name: 'Lenovo',
+// }
+//we may change person's properties so we don't create absolute constantas in Js
+//Important part of variables is difference between let and var
+//Data types
+//undefined,null,string,number,object,array,boolean others are objects
+//if we  create variable and it is empty  machine returns undefined if we need special empty variable  so we give null to it
+// let freeze
+// let cool = null
+// console.log(freeze, cool)
+//interpolation is that we use bacticks
+// console.log(cool + 1 + freeze)
+//conditions
+// let trafficlight = 'red'
+// switch (trafficlight) {
+// 	case 'green':
+// 		console.log('may') //first condition wroks and others stop
+// 		break
+// 	case 'red':
+// 		console.log('may')
+// 		break
+// 	case 'green':
+// 		console.log('may2')
+// 		break
+// }
+//if else is strictly than switch case
+//Cycles
+//when we need steps similar to others we use cycles
+let number = 2
+while (number > 6) {
+	console.log(1)
+	number++
+}
